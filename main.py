6ac1f0df8fa5eabc7a02e23ad42995825fb8fb9c
Aditya Pal <your_github_email@example.com>
2026-01-08 00:17:16 +0530

    Ready for Render deployment

--- main.py
+++ main.py
@@ -99,6 +99,6 @@ def logout():
     session.pop('logged_in', None)
     return redirect(url_for('login'))
 
-# if __name__ == '__main__':
-#     app.run(debug=True, port=5000)
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
 
